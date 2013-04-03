f755de196657f3c65096012b863590807b3ded78
Pedro Markun <pedro@esfera.mobi>
2013-04-03 16:30:10 -0300

    adicionado o controle de zoom, esta escondido embaixo dos labels. separado o codigo do mapa na home para o mapbox.php fix #2

---
+++ mapbox.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Mapa da Home
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ */
+?>
+<div id="map" class="img-polaroid">
+	<a href="#" class="zoomer zoomin">+</a>
+	<a href="#" class="zoomer zoomout">-</a>
+	<ul id="map-ui">
+		<a href='#' id='acaoeducativa.mapadosplanos' class='active'>Munícipios</a>
+		<a href='#' id='acaoeducativa.mapadosplanos-estados'>Estados</a>
+	</ul>
+	<div class="map-legends">
+		<div class="map-legend">
+			<h2>Planos de Educação</h2>
+			<div class="legenda">
+				<div class="leg_elaboracao">
+					<span class="marker"></span>
+					<span>Em elaboração</span>
+				</div>
+				<div class="leg_complano">
+					<span class="marker"></span>
+					<span>Com plano</span>
+				</div>
+				<div class="leg_semplano">
+					<span class="marker"></span>
+					<span>Sem plano</span>
+				</div>
+				<div class="leg_gestor">
+					<span class="marker"></span>
+					<span>Resposta do/a gestor/a</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
