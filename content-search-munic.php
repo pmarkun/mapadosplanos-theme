70a84776fc2987ccec42872ee04f1ac9ad0cb2a8
Pedro Markun <pedro@esfera.mobi>
2013-05-02 10:37:08 -0300

    adicionando nome do munic na busca fix #73

--- content-search-munic.php
+++ content-search-munic.php
@@ -20,18 +20,9 @@
 			<div class="entry-content pull-left">
 			<img src="http://api.tiles.mapbox.com/v3/acaoeducativa.mapadosplanos/<?php echo $custom_fields['lng'][0] . "," . $custom_fields['lat'][0]; ?>,8/90x90.png" />
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-	
-	
-	
-	
-		<header class="entry-header media-body">
-			<h1 class="entry-title">
+			<h3 class="entry-title">
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-			</h1>
-		</header><!-- .entry-header -->
-
-
-		
+			</h3>
+		</div><!-- .entry-content -->
 
 </article><!-- #post -->
