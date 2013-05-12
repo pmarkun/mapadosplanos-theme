6a32b23bbf3eaa54fddeb3184c4cdee24c9b7e7b
Daniela B. Silva <danielabsilva@gmail.com>
2013-05-12 22:21:01 +0200

    incluindo dados na visualizacao NaoTemPlano

--- content-municipio.php
+++ content-municipio.php
@@ -306,6 +306,20 @@
 						<td><span class="resposta label label-info ibge-<?php echo types_render_field('qs_plano26'); ?>"><?php echo types_render_field('qs_plano26'); ?></span></td>
 					</tr>
 				</table>
+				<table class="table table-bordered">
+					<tr><th colspan="2">Plano de Educação</th></tr>
+					<tr>
+						<td>Tem plano?</td>
+						<td><span class="resposta label label-info ibge-<?php echo types_render_field('qs_etapa01'); ?>"><?php echo types_render_field('qs_etapa01'); ?></span></td>
+					</tr>
+					<?php if ($etapa == 'complano') : ?>
+					<tr>
+						<td>Ano desta gestão em que pretende elaborá-lo</td>
+						<td><span class="resposta label label-info ibge-Multi"><?php echo types_render_field('qs_plano23'); ?></span></td>
+					</tr>
+					</table>
+					<?php endif; ?>
+				</table>
 			<?php else : ?>
 				<span class="titulo"><p>O(A) Gestor(a) da área de educação de seu município ainda não compartilhou informações sobre o processo de construção do Plano de Educação local.</p></span>
 				<p>Se você é gestor, colabore conosco nesta coleta de informações e <a href="<?php bloginfo('wpurl');?>/wp-admin/post.php?post=<?php the_ID(); ?>&action=edit">preencha o questionário</a> sobre a elaboração do Plano de Educação do seu Município. </p>
